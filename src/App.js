6a21b75a960f622a5138c254989d4f39c50ccfb8
jsxgod <smykoo7@gmail.com>
2021-09-29 13:36:14 +0200

    Render home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import "./sass/main.scss";
+import { Home } from "./pages"
+import { NavBar } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <NavBar />
+      <Home />
+    </Router>
+ 
   );
 }
 

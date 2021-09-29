07c621823a2351198f2583d4022063af6e38210d
jsxgod <smykoo7@gmail.com>
2021-09-29 15:39:59 +0200

    Store data in a seperate file

---
+++ src/data/modelData.js
@@ -0,0 +1,44 @@
+const modelData = {
+  "template1": {
+    name: "Name",
+    surname: "Surname",
+    longitude: "28.538336",
+    latitude: "-81.379234",
+    image: `/images/models/model-1-small.jpg`,
+    instagram: `@instagram_placeholder`
+  },
+  "template2": {
+    name: "Name_2",
+    surname: "Surname_2",
+    longitude: "28.538336",
+    latitude: "-81.379234",
+    image: `/images/models/model-1-small.jpg`,
+    instagram: `@instagram_placeholder`
+  },
+  "template3": {
+    name: "Name_3",
+    surname: "Surname_3",
+    longitude: "28.538336",
+    latitude: "-81.379234",
+    image: `/images/models/model-1-small.jpg`,
+    instagram: `@instagram_placeholder`
+  },
+  "template4": {
+    name: "Name_4",
+    surname: "Surname_4",
+    longitude: "28.538336",
+    latitude: "-81.379234",
+    image: `/images/models/model-1-small.jpg`,
+    instagram: `@instagram_placeholder`
+  },
+  "template5": {
+    name: "Name_5",
+    surname: "Surname_5",
+    longitude: "28.538336",
+    latitude: "-81.379234",
+    image: `/images/models/model-1-small.jpg`,
+    instagram: `@instagram_placeholder`
+  },
+}
+
+export default modelData;

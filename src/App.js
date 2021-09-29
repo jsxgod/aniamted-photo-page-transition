1dae5ccfdc8b150242604b911a61b47426fc0c63
jsxgod <smykoo7@gmail.com>
2021-09-29 18:02:57 +0200

    Render model component. Add routing.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./sass/main.scss";
-import { Home } from "./pages"
+import { Home, Model } from "./pages"
 import { NavBar } from "./components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -8,7 +8,22 @@ function App() {
   return (
     <Router>
       <NavBar />
-      <Home />
+      <Route
+      render={({location}) => (
+        <Switch location={location} key={location.pathname}>
+          <Route
+          exact
+          path="/"
+          render={() => <Home />}
+          />
+          <Route
+          exact
+          path="/model/:id"
+          render={() => <Model />}
+          />
+        </Switch>
+      )}
+      />
     </Router>
  
   );

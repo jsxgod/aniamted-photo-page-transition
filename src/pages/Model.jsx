7326c51e44b5154985f5108d1cb583e9a32b9d2c
jsxgod <smykoo7@gmail.com>
2021-09-29 18:01:28 +0200

    Create Model component

---
+++ src/pages/Model.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Model = () => {
+  return (
+    <main>
+      <div className="info-container">
+        <div className="model-info">
+          <div className="location">
+            <span>28.538336</span>
+            <span>-81.379234</span>
+          </div>
+          <div className="instagram">@instagram_placeholder</div>
+        </div>
+        <div className="name-container">
+          <span className="first">
+            <span>Y</span>
+            <span>a</span>
+            <span>s</span>
+            <span>m</span>
+            <span>e</span>
+            <span>e</span>
+            <span>n</span>
+          </span>
+          <span className="last">
+            <span>T</span>
+            <span>a</span>
+            <span>r</span>
+            <span>i</span>
+            <span>q</span>
+          </span>
+        </div>
+      </div>
+      <div className="big-image-container">
+        <img
+          src={process.env.PUBLIC_URL + `/images/models/model-1.webp`}
+          alt="model1.jpg"
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Model;

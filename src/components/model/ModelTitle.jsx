d177ea4c13b56ab2e3725112225ce2146bb25fbc
jsxgod <smykoo7@gmail.com>
2021-10-04 14:58:11 +0200

    Animate model title

--- src/components/model/ModelTitle.jsx
+++ src/components/model/ModelTitle.jsx
@@ -2,10 +2,10 @@ import { motion } from "framer-motion";
 import React from "react";
 
 const variants = {
-  initial: {
+  hidden: {
     opacity: 0,
   },
-  animate: {
+  visible: {
     opacity: 1,
     transition: {
       delay: 1.4,
@@ -16,33 +16,80 @@ const variants = {
   exit: {},
 };
 
+const wordVariants = {
+  animate: {
+    transition: {
+      delayChildren: 1.4,
+      staggerChildren: 0.04,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const wordVariants2 = {
+  animate: {
+    transition: {
+      delayChildren: 1.4,
+      staggerChildren: 0.04,
+      staggerDirection: 1,
+    },
+  },
+};
+
+const letterVariants = {
+  initial: {
+    y: "100%",
+  },
+  animate: {
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: [0.43, 0.13, 0.23, 0.96],
+    },
+  },
+};
+
 const ModelTitle = ({ model }) => {
   return (
     <div className="title-container">
       <motion.div
         className="title-info-container"
         variants={variants}
-        initial="initial"
-        animate="animate"
+        initial="hidden"
+        animate="visible"
       >
         <div className="instagram">{model.instagram}</div>
       </motion.div>
       <motion.div
         className="name-container"
         variants={variants}
-        initial="initial"
-        animate="animate"
+        initial="hidden"
+        animate="visible"
       >
-        <span className="first-name">
-          {[...model.name].map((char) => (
-            <span>{char}</span>
+        <motion.span
+          variants={wordVariants}
+          initial="initial"
+          animate="animate"
+          className="first-name"
+        >
+          {[...model.name].map((char, i) => (
+            <motion.span key={i} variants={letterVariants}>
+              {char}
+            </motion.span>
           ))}
-        </span>
-        <span className="last-name">
-          {[...model.surname].map((char) => (
-            <span>{char}</span>
+        </motion.span>
+        <motion.span
+          variants={wordVariants2}
+          initial="initial"
+          animate="animate"
+          className="last-name"
+        >
+          {[...model.surname].map((char, i) => (
+            <motion.span key={i} variants={letterVariants}>
+              {char}
+            </motion.span>
           ))}
-        </span>
+        </motion.span>
       </motion.div>
     </div>
   );

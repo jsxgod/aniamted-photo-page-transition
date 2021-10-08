210c9cebd6506d9e10fad8957ee903781d1b800a
jsxgod <smykoo7@gmail.com>
2021-10-08 23:26:07 +0200

    Change curtain behavior and animations

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -12,44 +12,48 @@ const Gallery = () => {
   }, []);
 
   return (
-    <motion.main
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 1 }}
-      transition={{
-        duration: 0.6,
-        ease: [0, 0.55, 0.45, 1],
-      }}
-    >
-      <div className="container">
-        <div className="gallery">
-          {modelData.map((model, i) => (
-            <div key={i} className="image-container">
-              <div className="thumbnail">
-                <Link
-                  to={`/model/${model.id}`}
-                  onClick={() => setModelClicked(true)}
-                  className="img-link"
-                >
-                  <img
-                    src={process.env.PUBLIC_URL + model.image}
-                    alt="model1.jpg"
-                  />
-                </Link>
-              </div>
-              <div className="information">
-                <div className="title">{model.name + " " + model.surname}</div>
-                <div className="location">
-                  <span>{model.longitude}</span>
-                  <span>{model.latitude}</span>
+    <>
+      <Curtain clicked={modelClicked} />
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: [0, 0.55, 0.45, 1],
+        }}
+      >
+        <div className="container">
+          <div className="gallery">
+            {modelData.map((model, i) => (
+              <div key={i} className="image-container">
+                <div className="thumbnail">
+                  <Link
+                    to={`/model/${model.id}`}
+                    onClick={() => setModelClicked(true)}
+                    className="img-link"
+                  >
+                    <img
+                      src={process.env.PUBLIC_URL + model.image}
+                      alt="model1.jpg"
+                    />
+                  </Link>
+                </div>
+                <div className="information">
+                  <div className="title">
+                    {model.name + " " + model.surname}
+                  </div>
+                  <div className="location">
+                    <span>{model.longitude}</span>
+                    <span>{model.latitude}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      <Curtain clicked={modelClicked} />
-    </motion.main>
+      </motion.main>
+    </>
   );
 };
 

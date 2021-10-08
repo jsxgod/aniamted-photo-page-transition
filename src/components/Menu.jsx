111f2f111bbcb529dc8ca4a8f00e86e3b5a7a0cd
jsxgod <smykoo7@gmail.com>
2021-10-08 23:24:58 +0200

    Create a component for the Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+const menuVariants = {
+  hide: {
+    display: "none",
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+      staggerChildren: 0.03,
+      staggerDirection: -1,
+    },
+  },
+  show: {
+    display: "grid",
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      ease: [0, 0.55, 0.45, 1],
+    },
+  },
+  exit: {},
+};
+
+const optionVariants = {
+  hide: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+      ease: [0, 0.55, 0.45, 1],
+    },
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      ease: [0, 0.55, 0.45, 1],
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+      ease: [0, 0.55, 0.45, 1],
+    },
+  },
+};
+
+const Menu = ({ opened }) => {
+  return (
+    <div className="menu-container">
+      <motion.div
+        className="menu"
+        variants={menuVariants}
+        initial="hide"
+        animate={opened ? "show" : "hide"}
+        exit="exit"
+      >
+        {true && (
+          <>
+            <motion.div className="option dark home" variants={optionVariants}>
+              <Link to="/">Home</Link>
+            </motion.div>
+            <motion.div
+              className="option bright about"
+              variants={optionVariants}
+            >
+              <Link to="/about-us">About</Link>
+            </motion.div>
+            <motion.div
+              className="option bright gallery"
+              variants={optionVariants}
+            >
+              <Link to="/gallery">Gallery</Link>
+            </motion.div>
+            <motion.div
+              className="option dark contact"
+              variants={optionVariants}
+            >
+              <Link to="/contact">Contact</Link>
+            </motion.div>
+            <motion.div className="option become" variants={optionVariants}>
+              <button className="menu-button">Become our model</button>
+            </motion.div>
+          </>
+        )}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Menu;

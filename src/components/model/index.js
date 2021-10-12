b2879d89039d5c47dc23270f885593008d564ecb
jsxgod <smykoo7@gmail.com>
2021-10-13 00:12:48 +0200

    Clean up imports

--- src/components/model/index.js
+++ src/components/model/index.js
@@ -1,3 +1,4 @@
 export { default as ModelTitle } from './ModelTitle';
 export { default as ModelImage } from './ModelImage';
 export { default as ModelInfo } from './ModelInfo';
+export { default as LightBox } from './LightBox';

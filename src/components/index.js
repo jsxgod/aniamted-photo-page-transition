689dc232943ff15f1363c5b1953fb47a6d18cf34
jsxgod <smykoo7@gmail.com>
2021-10-13 14:44:49 +0200

    Clean up imports

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Curtain } from './Curtain';
 export { default as Menu } from './Menu';
 export { default as HamburgerButton } from './HamburgerButton';
 export { default as HamburgerRail } from './HamburgerRail';
+export { default as SvgArrow } from './SvgArrow';

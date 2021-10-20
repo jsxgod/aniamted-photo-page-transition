ba06d1ae6550800cb58d3c185045246ca686dfeb
jsxgod <smykoo7@gmail.com>
2021-10-20 22:24:08 +0200

    Clean up imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Gallery } from "./Gallery";
 export { default as Model } from "./Model";
+export { default as Contact } from "./Contact";

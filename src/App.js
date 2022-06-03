a6ebdcd0f0fa7f5bd45b599b3332761eb1b6de1c
Kacper Smyczyk <smykoo7@gmail.com>
2022-06-03 03:42:00 +0200

    Format file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./sass/main.scss";
-import { Home,  Gallery, Model, Contact } from "./pages"
+import { Home, Gallery, Model, Contact } from "./pages";
 import { NavBar } from "./components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
@@ -9,42 +9,36 @@ function App() {
   return (
     <Router>
       <Route
-      render={({location}) => (
-        <AnimatePresence exitBeforeEnter>
-        <Switch location={location} key={location.pathname}>
-          <Route
-          exact
-          path="/"
-          render={() => <Home />}
-          />
-          <Route
-          exact
-          path="/gallery"
-          render={() => 
-            <>
-              <NavBar />
-              <Gallery />
-            </>}
-          />
-          <Route
-          exact
-          path="/model/:id"
-          render={() => 
-          <>
-            <NavBar showBackArrow/>
-            <Model />
-          </>}
-          />
-          <Route
-          exact
-          path="/contact-us"
-          render={() => <Contact />}/>
-        </Switch>
-        </AnimatePresence>
-      )}
+        render={({ location }) => (
+          <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/" render={() => <Home />} />
+              <Route
+                exact
+                path="/gallery"
+                render={() => (
+                  <>
+                    <NavBar />
+                    <Gallery />
+                  </>
+                )}
+              />
+              <Route
+                exact
+                path="/model/:id"
+                render={() => (
+                  <>
+                    <NavBar showBackArrow />
+                    <Model />
+                  </>
+                )}
+              />
+              <Route exact path="/contact-us" render={() => <Contact />} />
+            </Switch>
+          </AnimatePresence>
+        )}
       />
     </Router>
- 
   );
 }
 
